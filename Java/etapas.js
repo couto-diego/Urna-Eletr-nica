08dd21a080568b6f6adde799ee9a4c99707ea35b
Diego Couto <dev.diegocouto@gmail.com>
2023-01-12 23:29:52 -0300

    Initial commit

---
+++ Java/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '35353',
+                nome: 'Flash',
+                partido: 'PJL',
+                fotos:[
+                    {url:'35353.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '55355',
+                nome: 'Loki',
+                partido: 'PHM',
+                fotos:[
+                    {url:'55355.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '35',
+                nome: 'Batman',
+                partido: 'PJL',
+                vice: 'Robin',
+                fotos:[
+                    {url:'35.png', legenda: 'Prefeito'},
+                    {url:'35_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '13',
+                nome: 'Coringa',
+                partido: 'PVM',
+                vice: 'Arlequina',
+                fotos:[
+                    {url:'13.png', legenda: 'Prefeito'},
+                    {url:'13_2.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
